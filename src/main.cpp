78f916c8a003f023bd389f9244934d6d42b6b6d8
teekuningas <erkkahe@gmail.com>
2024-07-01 20:04:03 +0300

    Fix all

--- src/main.cpp
+++ src/main.cpp
@@ -21,23 +21,23 @@ public:
 	bool hyppy;
 	void MuutaX(GLfloat xpar)
 	{
-		x=xpar;
+		x = xpar;
 	}
 	void MuutaY(GLfloat ypar)
 	{
-		y=ypar;
+		y = ypar;
 	}
 	void MuutaVY(GLfloat vypar)
 	{
-		vy=vypar;
+		vy = vypar;
 	}
 	void MuutaVX(GLfloat vxpar)
 	{
-		vx=vxpar;
+		vx = vxpar;
 	}
 	void MuutaAlkupY(GLfloat alkupypar)
 	{
-		alkupy=alkupypar;
+		alkupy = alkupypar;
 	}
 	GLfloat LueX()
 	{
@@ -59,7 +59,6 @@ public:
 	{
 		return alkupy;
 	}
-
 };
 
 class Tausta
@@ -71,11 +70,11 @@ public:
 	SDL_Surface *image;
 	void MuutaX(GLfloat xpar)
 	{
-		x=xpar;
+		x = xpar;
 	}
 	void MuutaY(GLfloat ypar)
 	{
-		y=ypar;
+		y = ypar;
 	}
 	GLfloat LueX()
 	{
@@ -87,61 +86,39 @@ public:
 	}
 };
 
-const int SYVYYS = 16;
-const int LEVEYS = 1024;
-const int KORKEUS = 768;
-const int IKKUNALIPPU = 0;
-const int METSA_LEVEYS = 8192;
-const int METSA_KORKEUS = 768;
-const GLfloat gravKiiht = 5;
+int hoida_viestit(void);
+void metsa_piirra(void);
+void metsa_toiminta(float deltaTime);
 
-int syvyys, kaksoispuskurointi;
+const int SYVYYS = 16;
+const int IKKUNA_LEVEYS = 1024;
+const int IKKUNA_KORKEUS = 768;
+const int KARTTA_LEVEYS = 2028;
+const int KARTTA_KORKEUS = 768;
+const int HAHMO_LEVEYS = 100;
+const int HAHMO_KORKEUS = 100;
 
 GLuint tex[4];
 
-int hoida_viestit(void);
-void metsa_piirra(void);
-void metsa_toiminta(void);
-
-const int fps = 20;
-int lastTick = 0;
-int tick = 0;
-
-
-
-Hahmo Sartre;
-Tausta tausta[1];
-
-int x, y;
+Hahmo sartre;
+GLuint texSartre[3];
+Tausta tausta;
+GLuint texTausta[1];
 
 int MySDL_glTexImage2D(SDL_Surface *kuva)
 {
-	SDL_Surface *apu;
-
 	if (!kuva || !kuva->w || !kuva->h || (kuva->w & 1) || (kuva->h & 1)) {
 		return -1;
 	}
 
-	apu = SDL_CreateRGBSurfaceWithFormat(0, kuva->w, kuva->h, 32, SDL_PIXELFORMAT_RGBA32);
-	if (!apu) {
+	SDL_Surface *formattedSurface = SDL_ConvertSurfaceFormat(kuva, SDL_PIXELFORMAT_RGBA32, 0);
+	if (!formattedSurface) {
 		return -1;
 	}
 
-	SDL_FillRect(apu, NULL, 0);
-	SDL_SetSurfaceAlphaMod(kuva, 0);
-	SDL_SetSurfaceBlendMode(kuva, SDL_BLENDMODE_NONE);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, formattedSurface->w, formattedSurface->h, 0, GL_RGBA, GL_UNSIGNED_BYTE, formattedSurface->pixels);
 
-	SDL_Rect r1, r2;
-	r1.x = r2.x = 0;
-	r1.h = r2.h = 1;
-	r1.w = r2.w = kuva->w;
-	for (r1.y = 0, r2.y = kuva->h - 1; r2.y >= 0; ++r1.y, --r2.y) {
-		SDL_BlitSurface(kuva, &r1, apu, &r2);
-	}
-
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, apu->w, apu->h, 0, GL_RGBA, GL_UNSIGNED_BYTE, apu->pixels);
-
-	SDL_FreeSurface(apu);
+	SDL_FreeSurface(formattedSurface);
 	return 0;
 }
 
@@ -164,83 +141,87 @@ void metsa_piirra(void)
 {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	glLoadIdentity();
-	glTranslatef(Sartre.LueX(), Sartre.LueY(), 0.0f);
+	glTranslatef(sartre.LueX(), sartre.LueY(), 0.0f);
+
 	glEnable(GL_BLEND);
 	glEnable(GL_ALPHA_TEST);
 
-	if (Sartre.hahmo == 0) glBindTexture(GL_TEXTURE_2D, tex[0]);
-	else if (Sartre.hahmo == 1) glBindTexture(GL_TEXTURE_2D, tex[1]);
-	else glBindTexture(GL_TEXTURE_2D, tex[2]);
+	if (sartre.hahmo == 0) glBindTexture(GL_TEXTURE_2D, texSartre[0]);
+	else if (sartre.hahmo == 1) glBindTexture(GL_TEXTURE_2D, texSartre[1]);
+	else glBindTexture(GL_TEXTURE_2D, texSartre[2]);
 
 	glBegin(GL_QUADS);
-	glTexCoord2f(0.0f, 1.0f);
-	glVertex3f(-100.0f, -100.0f, 1.0f); // Bottom Left
-	glTexCoord2f(1.0f, 1.0f);
-	glVertex3f(100.0f, -100.0f, 1.0f); // Bottom Right
+	glTexCoord2f(0.0f, -1.0f);
+	glVertex3f(-HAHMO_LEVEYS, -HAHMO_KORKEUS, 1.0f); // Bottom Left
+	glTexCoord2f(1.0f, -1.0f);
+	glVertex3f(HAHMO_LEVEYS, -HAHMO_KORKEUS, 1.0f); // Bottom Right
 	glTexCoord2f(1.0f, 0.0f);
-	glVertex3f(100.0f, 100.0f, 1.0f); // Top Right
+	glVertex3f(HAHMO_LEVEYS, HAHMO_KORKEUS, 1.0f); // Top Right
 	glTexCoord2f(0.0f, 0.0f);
-	glVertex3f(-100.0f, 100.0f, 1.0f); // Top Left
+	glVertex3f(-HAHMO_LEVEYS, HAHMO_KORKEUS, 1.0f); // Top Left
 	glEnd();
 
 	glDisable(GL_BLEND);
 	glDisable(GL_ALPHA_TEST);
 
 	glLoadIdentity();
-	glTranslatef(tausta[0].LueX(), tausta[0].LueY(), 0.0f);
-	glBindTexture(GL_TEXTURE_2D, tex[3]);
+
+	glTranslatef(tausta.LueX(), tausta.LueY(), 0.0f);
+	glBindTexture(GL_TEXTURE_2D, texTausta[0]);
 	glBegin(GL_QUADS);
-	glTexCoord2f(0.0f, 0.0f);
-	glVertex3f(0.0f, KORKEUS, 0.0f); // Bottom Left
+	// Cropataan vähän reunasta
+	glTexCoord2f(0.01f, 0.0f);
+	glVertex3f(-KARTTA_LEVEYS*2, KARTTA_KORKEUS, 0.0f); // Bottom Left
 	glTexCoord2f(1.0f, 0.0f);
-	glVertex3f(LEVEYS, KORKEUS, 0.0f); // Bottom Right
-	glTexCoord2f(1.0f, 1.0f);
-	glVertex3f(LEVEYS, 0.0f, 0.0f); // Top Right
-	glTexCoord2f(0.0f, 1.0f);
-	glVertex3f(0.0f, 0.0f, 0.0f); // Top Left
+	glVertex3f(KARTTA_LEVEYS*2, KARTTA_KORKEUS, 0.0f); // Bottom Right
+	glTexCoord2f(1.0f, -1.0f);
+	glVertex3f(KARTTA_LEVEYS*2, 0.0f, 0.0f); // Top Right
+	// Cropataan vähän reunasta
+	glTexCoord2f(0.01f, -1.0f);
+	glVertex3f(-KARTTA_LEVEYS*2, 0.0f, 0.0f); // Top Left
 	glEnd();
 }
 
-void metsa_toiminta(void)
+void metsa_toiminta(float deltaTime)
 {
 	const Uint8 *keystate = SDL_GetKeyboardState(NULL);
 
 	if (keystate[SDL_SCANCODE_RIGHT]) {
-		if (tausta[0].LueX() > -3072) {
-			if (Sartre.LueX() < ((LEVEYS / 4) * 3)) {
-				Sartre.MuutaX(Sartre.LueX() + Sartre.LueVX());
-			} else tausta[0].MuutaX(tausta[0].LueX() - Sartre.LueVX());
+		if (tausta.LueX() > -(KARTTA_LEVEYS*2 - IKKUNA_LEVEYS)) {
+			if (sartre.LueX() < ((IKKUNA_LEVEYS / 4) * 3)) {
+				sartre.MuutaX(sartre.LueX() + deltaTime*sartre.LueVX());
+			} else tausta.MuutaX(tausta.LueX() - deltaTime*sartre.LueVX());
 		} else {
-			if (Sartre.LueX() < LEVEYS - 100) {
-				Sartre.MuutaX(Sartre.LueX() + Sartre.LueVX());
+			if (sartre.LueX() < IKKUNA_LEVEYS - 100) {
+				sartre.MuutaX(sartre.LueX() + deltaTime*sartre.LueVX());
 			}
 		}
 	}
 
 	if (keystate[SDL_SCANCODE_LEFT]) {
-		if (tausta[0].LueX() < 4096) {
-			if (Sartre.LueX() > (LEVEYS / 4)) {
-				Sartre.MuutaX(Sartre.LueX() - Sartre.LueVX());
-			} else tausta[0].MuutaX(tausta[0].LueX() + Sartre.LueVX());
+		if (tausta.LueX() < KARTTA_LEVEYS*2) {
+			if (sartre.LueX() > (IKKUNA_LEVEYS / 4)) {
+				sartre.MuutaX(sartre.LueX() - deltaTime*sartre.LueVX());
+			} else tausta.MuutaX(tausta.LueX() + deltaTime*sartre.LueVX());
 		} else {
-			if (Sartre.LueX() > 0 + 100) {
-				Sartre.MuutaX(Sartre.LueX() - Sartre.LueVX());
+			if (sartre.LueX() > 0 + 100) {
+				sartre.MuutaX(sartre.LueX() - deltaTime*sartre.LueVX());
 			}
 		}
 	}
 
-	if (Sartre.hyppy == 0 && keystate[SDL_SCANCODE_UP]) {
-		Sartre.hyppy = 1;
-		Sartre.MuutaVY(-50);
-		Sartre.MuutaAlkupY(Sartre.LueY());
+	if (sartre.hyppy == 0 && keystate[SDL_SCANCODE_UP]) {
+		sartre.hyppy = 1;
+		sartre.MuutaVY(-1200);
+		sartre.MuutaAlkupY(sartre.LueY());
 	}
 
-	if (Sartre.hyppy == 1) {
-		Sartre.MuutaY(Sartre.LueY() + Sartre.LueVY());
-		Sartre.MuutaVY(Sartre.LueVY() + gravKiiht);
-		if (Sartre.LueY() > Sartre.LueAlkupY()) {
-			Sartre.hyppy = 0;
-			Sartre.MuutaY(Sartre.LueAlkupY());
+	if (sartre.hyppy == 1) {
+		sartre.MuutaY(sartre.LueY() + deltaTime*sartre.LueVY());
+		sartre.MuutaVY(sartre.LueVY() + deltaTime*3000);
+		if (sartre.LueY() > sartre.LueAlkupY()) {
+			sartre.hyppy = 0;
+			sartre.MuutaY(sartre.LueAlkupY());
 		}
 	}
 }
@@ -248,6 +229,7 @@ void metsa_toiminta(void)
 
 int main(int argc, char **argv)
 {
+
 	if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER) != 0) {
 		printf("Virhe: SDL_Init: %s\n", SDL_GetError());
 		exit(1);
@@ -259,10 +241,10 @@ int main(int argc, char **argv)
 	SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
 
 	SDL_Window *window = SDL_CreateWindow(
-	                         "Sartre temmeltaa lehdossa nalkaisena",
+	                         "Sartre temmeltaa lehdossa nälkaisenä",
 	                         SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
-	                         LEVEYS, KORKEUS,
-	                         SDL_WINDOW_OPENGL | IKKUNALIPPU
+	                         IKKUNA_LEVEYS, IKKUNA_KORKEUS,
+	                         SDL_WINDOW_OPENGL | 0
 	                     );
 
 	if (!window) {
@@ -277,25 +259,11 @@ int main(int argc, char **argv)
 		exit(2);
 	}
 
-	SDL_GL_GetAttribute(SDL_GL_DEPTH_SIZE, &syvyys);
-	SDL_GL_GetAttribute(SDL_GL_DOUBLEBUFFER, &kaksoispuskurointi);
-
-	if (syvyys > SYVYYS) {
-		printf("Syvyys on toivottua suurempi \\o/. Pyydettiin %d, saatiinkin %d...\n", SYVYYS, syvyys);
-	} else if (syvyys < SYVYYS) {
-		printf("Syvyys on toivottua pienempi :(. Haluttiin %d, saatiinkin %d...\n", SYVYYS, syvyys);
-	}
-
-	if (!kaksoispuskurointi) {
-		printf("Ei ole kaksoispuskuria! Qu'est-ce que c'est que cela? O_o\n");
-		printf("Nyt voi sattua hassuja...\n");
-	}
-
-	glViewport(0, 0, LEVEYS, KORKEUS);
+	glViewport(0, 0, IKKUNA_LEVEYS, IKKUNA_KORKEUS);
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
 
-	glOrtho(0.0f, LEVEYS, KORKEUS, 0.0f, -100.0f, 100.0f);
+	glOrtho(0.0f, IKKUNA_LEVEYS, IKKUNA_KORKEUS, 0.0f, -100.0f, 100.0f);
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
 	glShadeModel(GL_SMOOTH);
@@ -311,87 +279,119 @@ int main(int argc, char **argv)
 	glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
 	glClearColor(0.0, 0.0, 0.0, 0.0);
 
-	Sartre.MuutaX(400.0);
-	Sartre.MuutaY(650.0);
-	Sartre.hahmo = 0;
-	Sartre.hyppy = 0;
-	Sartre.MuutaVX(16);
-
-	lastTick = SDL_GetTicks();
-
+	// Kuvat
 	SDL_RWops *rwop;
 	rwop = SDL_RWFromFile("data/images/Sartre1a.png", "rb");
-	Sartre.image[0] = IMG_LoadPNG_RW(rwop);
+	sartre.image[0] = IMG_LoadPNG_RW(rwop);
 	SDL_RWclose(rwop);
 
 	rwop = SDL_RWFromFile("data/images/Sartre2a.png", "rb");
-	Sartre.image[1] = IMG_LoadPNG_RW(rwop);
+	sartre.image[1] = IMG_LoadPNG_RW(rwop);
 	SDL_RWclose(rwop);
 
 	rwop = SDL_RWFromFile("data/images/Sartre3a.png", "rb");
-	Sartre.image[2] = IMG_LoadPNG_RW(rwop);
+	sartre.image[2] = IMG_LoadPNG_RW(rwop);
 	SDL_RWclose(rwop);
 
-	rwop = SDL_RWFromFile("data/images/Lehto.png", "rb");
-	tausta[0].image = IMG_LoadPNG_RW(rwop);
+	rwop = SDL_RWFromFile("data/images/lehto.png", "rb");
+	tausta.image = IMG_LoadPNG_RW(rwop);
 	SDL_RWclose(rwop);
 
-	glGenTextures(4, tex);
+	glGenTextures(3, texSartre);
+	glGenTextures(1, texTausta);
 
-	glBindTexture(GL_TEXTURE_2D, tex[0]);
+	glBindTexture(GL_TEXTURE_2D, texSartre[0]);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-	MySDL_glTexImage2D(Sartre.image[0]);
+	MySDL_glTexImage2D(sartre.image[0]);
 
-	glBindTexture(GL_TEXTURE_2D, tex[1]);
+	glBindTexture(GL_TEXTURE_2D, texSartre[1]);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-	MySDL_glTexImage2D(Sartre.image[1]);
+	MySDL_glTexImage2D(sartre.image[1]);
 
-	glBindTexture(GL_TEXTURE_2D, tex[2]);
+	glBindTexture(GL_TEXTURE_2D, texSartre[2]);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-	MySDL_glTexImage2D(Sartre.image[2]);
+	MySDL_glTexImage2D(sartre.image[2]);
 
-	glBindTexture(GL_TEXTURE_2D, tex[3]);
+	glBindTexture(GL_TEXTURE_2D, texTausta[0]);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-	MySDL_glTexImage2D(tausta[0].image);
+	MySDL_glTexImage2D(tausta.image);
+
+	// Musiikki
+
+	if (SDL_Init(SDL_INIT_AUDIO) < 0) {
+		printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+		return -1;
+	}
+
+	if (Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 2048) < 0) {
+		printf("SDL_mixer could not initialize! SDL_mixer Error: %s\n", Mix_GetError());
+		return -1;
+	}
+
+	Mix_Music *backgroundMusic = Mix_LoadMUS("data/music/Myytti.mp3");
+	if (backgroundMusic == NULL) {
+		printf("Failed to load background music! SDL_mixer Error: %s\n", Mix_GetError());
+		return -1;
+	}
+
+	// Play the MP3 file in a loop
+	if (Mix_PlayMusic(backgroundMusic, -1) == -1) {
+		printf("Failed to play background music! SDL_mixer Error: %s\n", Mix_GetError());
+		return -1;
+	}
+
+	// Alusta hahmo
+	sartre.MuutaX(400.0);
+	sartre.MuutaY(650.0);
+	sartre.hahmo = 0;
+	sartre.hyppy = 0;
+	sartre.MuutaVX(500);
 
 	if (hoida_viestit() != 0) {
 		exit(0);
 	}
 
+	Uint32 lastTick = SDL_GetTicks();
+	Uint32 currentTick = 0;
+	Uint32 hahmoElapsed = 0;
+	float deltaTime = 0.0f;
+
 	while (hoida_viestit() == 0) {
-		tick = SDL_GetTicks();
+		currentTick = SDL_GetTicks();
+		deltaTime = (currentTick - lastTick) / 1000.0f;
+		hahmoElapsed += currentTick - lastTick;
+		lastTick = currentTick;
 
-		if (tick <= lastTick) {
-			SDL_Delay(5);
-			continue;
+		// Laske liikkeet
+		metsa_toiminta(deltaTime);
+
+		// Vaihda hahmoa
+		if (hahmoElapsed >= 300) {
+			sartre.hahmo = (sartre.hahmo + 1) % 3;
+			hahmoElapsed = 0;
 		}
 
-		if (lastTick >= tick)
-			continue;
-
-		while (lastTick < tick) {
-			metsa_toiminta();
-			lastTick += (1000 / fps);
-		}
-
+		// Piirrä
 		metsa_piirra();
-		if (kaksoispuskurointi) {
-			SDL_GL_SwapWindow(window);
-		}
+		SDL_GL_SwapWindow(window);
+
+
+		SDL_Delay(1);
 	}
 
 	for (int a = 0; a < 3; a++) {
-		SDL_FreeSurface(Sartre.image[a]);
+		SDL_FreeSurface(sartre.image[a]);
 	}
-	SDL_FreeSurface(tausta[0].image);
+	SDL_FreeSurface(tausta.image);
 
 	for (int a = 0; a < 4; a++) {
-		glDeleteTextures(1, &tex[a]);
+		glDeleteTextures(1, &texSartre[a]);
 	}
+	glDeleteTextures(1, &texTausta[0]);
 
 	SDL_GL_DeleteContext(context);
 	SDL_DestroyWindow(window);
@@ -400,5 +400,3 @@ int main(int argc, char **argv)
 
 	return 0;
 }
-
-
