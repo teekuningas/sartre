6b83351b709b101ce6b31d50ecdf9a5396964b77
teekuningas <erkkahe@gmail.com>
2024-09-19 00:45:09 +0300

    Try packaging the app to be run self-sufficiently with nix

--- flake.nix
+++ flake.nix
@@ -6,23 +6,54 @@
   };
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = nixpkgs.legacyPackages.${system};
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        buildInputs = with pkgs; [
+          SDL2.dev
+          SDL2_image
+          SDL2_mixer.dev
+          SDL2_ttf
+          xorg.libX11
+          gcc
+          astyle
+          alsa-lib
+        ];
+        ldLibraryPath = pkgs.lib.makeLibraryPath buildInputs;
+        cpath = "${pkgs.SDL2.dev}/include/SDL2:${pkgs.SDL2_image}/include/SDL2:${pkgs.SDL2_mixer.dev}/include/SDL2:${pkgs.SDL2_ttf}/include/SDL2";
       in {
         formatter = pkgs.nixfmt;
-        devShells.default = pkgs.mkShell rec {
-          buildInputs = with pkgs; [
-            SDL2.dev
-            SDL2_image
-            SDL2_mixer.dev
-            SDL2_ttf
-            xorg.libX11
-            gcc
-            astyle
-            alsa-lib
-          ];
-          LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath buildInputs;
-          CPATH = "${pkgs.SDL2.dev}/include/SDL2:${pkgs.SDL2_image}/include/SDL2:${pkgs.SDL2_mixer.dev}/include/SDL2:${pkgs.SDL2_ttf}/include/SDL2";
+
+        devShells.default = pkgs.mkShell {
+          buildInputs = buildInputs;
+          LD_LIBRARY_PATH = ldLibraryPath;
+          CPATH = cpath;
+        };
+
+        packages.default = pkgs.stdenv.mkDerivation {
+          pname = "sartre";
+          version = "0.1.0";
+
+          src = ./.;
+
+          buildInputs = buildInputs;
+
+          buildPhase = ''
+            export LD_LIBRARY_PATH=${ldLibraryPath}
+            export CPATH=${cpath}
+            mkdir -p obj
+            make
+          '';
+
+          installPhase = ''
+            mkdir -p $out/bin
+            cp main $out/bin/sartre
+          '';
+
+        };
+
+        defaultApp = {
+          type = "app";
+          program = "${self.packages.${system}.default}/bin/sartre";
         };
       });
 }
-
